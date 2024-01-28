ac2dffb64b7100a2fee5637d34eb8bf3d3656952
linajin03 <53534844+linajin03@users.noreply.github.com>
2024-01-28 01:33:19 -0500

    removing app and adding some CSS

--- my-app/src/AppRoutes.js
+++
@@ -1,9 +0,0 @@
-import Home from './Pages/Homepage.js';
-import Projects from './Components/Highlight.jsx';
-
-const AppRoutes = [
-  { path: '/', component: <Home />, exact: true },
-  { path: '/projects', component: <Projects /> }
-];
-
-export default AppRoutes;
